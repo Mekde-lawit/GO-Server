c4544191097e06ff280954dbf69dc6b8b3505011
Mekdelawit Belay <meqdellawittesfu@gmail.com>
2026-07-01 19:41:46 +0300

    add service

--- JWT/services/auth.service.go
+++ JWT/services/auth.service.go
@@ -1,9 +1,22 @@
 package services
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
-func HashPassword() string {
-	return ""
+// hashes the user password
+func HashPassword(password string) []byte {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil
+	}
+  return hashedPassword
 }
-func VerifyPasswod() bool {
-	return true
+
+// checks if the provided password matches
+func VerifyPasswod(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword),
+		[]byte(password),
+	)
 }
